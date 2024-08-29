16049e4628209803402145d0cbe97cfbce46f4bf
parth2374 <143696865+parth2374@users.noreply.github.com>
2024-08-29 22:59:01 +0530

    Update LogoPreview.jsx

--- src/components/LogoPreview.jsx
+++ src/components/LogoPreview.jsx
@@ -62,7 +62,7 @@ function LogoPreview({ downloadIcon }) {
                 >
                     { storageValue?.icon?.includes('.png') ?
                         <img
-                            src={'/png/' + storageValue?.icon}
+                            src={BASE_URL + '/png/' + storageValue?.icon}
                             style={{
                                 height: storageValue?.iconSize,
                                 width: storageValue?.iconSize,
